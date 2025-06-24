9c50ed627ca8941831b9b70a4d0a35472e62d70d
methan6427 <adamkh0698@gmail.com>
2025-06-25 00:05:27 +0300

    d

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/Adam-Khabisa-Port/' : '/',
   server: {
     host: "::",
     port: 8080,
